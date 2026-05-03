3b1f23a2dbdede1b1bd12ba09189e7bc1d6847b3
Thunder <69bobbywobby@gmail.com>
2026-05-03 03:18:10 -0400

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,10 +5,8 @@ author 'Thunder'
 description 'Scoreboard with scaling'
 version '1.0.0'
 
--- This tells FiveM WHERE the UI is
 ui_page 'html/index.html'
 
--- This tells FiveM to actually LOAD these files into the game's memory
 files {
     'html/index.html',
     'html/style.css'
